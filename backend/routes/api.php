4029e35b4e3c840826e41a46a4c509450fede91b
DumarMatamoros <e1315644771@live.uleam.edu.ec>
2025-11-13 20:17:01 -0500

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\FacturaController;
 use App\Http\Controllers\Api\ConfiguracionController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 // Rutas públicas (sin autenticación)
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
